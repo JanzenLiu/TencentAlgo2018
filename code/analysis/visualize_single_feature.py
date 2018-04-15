d8d8f19d34a7c67d9199a4401288281e5b397802
JanzenLiu <chengtsung_liu@yahoo.com>
2018-04-16 00:55:55 +0800

    Add functions to visualize single categorical feature

--- code/analysis/visualize_single_feature.py
+++ code/analysis/visualize_single_feature.py
@@ -1,6 +1,36 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import Colormap  # for type annotation
+
+
+def get_cmap(n, name='hsv') -> Colormap:
+    """Construct a `Colormap` instance that maps integer indices to distinct colors
+
+    Adapted from https://stackoverflow.com/questions/14720331/how-to-generate-random-colors-in-matplotlib
+
+    Parameters
+    ----------
+    n: int
+        The number of entries desired to put in the `Colormap` instance
+
+    name: string
+        The standard mpl colormap name to construct the `Colormap` instance with
+
+    Returns
+    -------
+    cm: Colormap
+        Colormap instance that maps each index in 0, 1, ..., n-1 to a distinct
+        RGB color.
+
+    Examples
+    --------
+    >>> cm = get_cmap(10)
+    >>> cm(5)  # a tuple representing an RGBA color
+    (0.0, 0.718752625002625, 1.0, 1.0)
+    """
+    cm = plt.cm.get_cmap(name, n)
+    return cm
 
 
 def plot_numerical_feat(x, name, ax=None, bins=None, **kw):
@@ -141,6 +171,172 @@ def plot_numerical_feats_double(dfs, num_cols=None, ncols=3, height_per_plot=6):
             ax.legend([df1_name, df2_name])
         ax.legend([df1_name, df2_name])
     for i in range(num_col_counts, nrows*ncols):
+        fig.delaxes(axs.flatten()[i])
+    plt.show()
+    print()
+
+
+def plot_categorical_feat(x, name, ax=None, keys=None, density=True, plot_nan=True, shift=None, **kw):
+    """Plot barplot for a single array representing a categorical feature
+
+    Parameters
+    ----------
+    x: array-like
+        Values of the feature. Free or not free from NaN are both acceptable.
+
+    name: string
+        Feature name to appear on the plot title.
+
+    ax: plt.Axes
+        `Axes` instance to plot the histogram on. If None, the histogram will
+        be plotted on plt directly. Please set this with a valid `Axes` if you
+        expect to plot the histogram in a subplot as a part of a larger
+        figure.
+
+    keys: array-like
+        Categorical values to be considered in the barplot. If None, all values
+        will be considered.
+
+    density: boolean
+        If True, the xticks will be set to the density of each categorical value,
+        instead of the absolute counts, default True.
+
+    plot_nan: boolean
+        If True, NaN will be also considered as a categorical values and will get
+        plotted in the barplot. default True.
+
+    shift: float
+        Horizontal shift of bars to their default positions. This should be set within
+        the range [0, 1).
+
+    Examples
+    --------
+    >>> df = pd.read_csv('train.csv')
+    >>> col = 'MSSubClass'  # column to plot, which is supposed to be categorical
+    >>> plot_categorical_feat(df[col], col)
+    """
+    nan_idx = pd.isnull(pd.Series(x))
+    x_clean = x[~nan_idx]  # filter out NaN values
+    if keys is None:
+        vals = x_clean.value_counts()  # category name-count map
+        keys = vals.index  # category names
+        counts = vals.values  # category counts
+    else:
+        vals = x_clean.value_counts()  # category name-count map
+        counts = np.array([vals.get(k, 0) for k in keys])  # category counts
+    if plot_nan:  # add counts for Nan values
+        keys = ['[NaN]'] + list(keys)
+        counts = np.array([np.sum(nan_idx)] + list(counts))
+    keys = [i + shift for i in range(len(keys))] if shift is not None else keys  # x coordinates if shift is needed
+    counts = counts / len(x) if density else counts  # density instead of absolute count if needed
+    title = "{} Distribution".format(name)
+    if ax is not None:
+        ax.bar(keys, counts, **kw)  # if ax is given, plot in it as a subplot
+        ax.set_title(title)
+    else:
+        plt.bar(keys, counts, **kw)  # if ax is not given, plot in plt globally
+        plt.title(title)
+
+
+def plot_categorical_feats(df, cat_cols=None, ncols=3, height_per_plot=6):
+    """Plot barplots for specified or simply all categorical features in a DataFrame
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        DataFrame whose numerical features you are going to plot.
+
+    cat_cols: array-like
+        Names of the categorical features to plot. If None, all features stored as
+        object type in the memory will be plotted.
+
+    ncols: int
+        Number of subplots in a row, default 3.
+
+    height_per_plot: int
+        Height of a subplot in the figure, default 6.
+
+    Examples
+    --------
+    >>> df = pd.read_csv("train.csv")
+    >>> plot_categorical_feats(df)
+    """
+    # df_name = get_var_name(df)  # problem of get_var_name is not fixed so far, so just comment it
+    # print(df_name)
+    # print("-" * len(df_name))
+    if cat_cols is None:
+        cat_cols = df.dtypes[df.dtypes == object].index.tolist()
+    num_col_counts = len(cat_cols)
+    nrows = int(np.ceil(num_col_counts / 3))
+    fig, axs = plt.subplots(nrows, ncols, figsize=(20, nrows * height_per_plot))
+    for i, feat in enumerate(cat_cols):
+        ix = i // ncols
+        iy = i % ncols
+        plot_categorical_feat(df[feat], feat, axs[ix][iy])
+    for i in range(num_col_counts, nrows * ncols):
+        fig.delaxes(axs.flatten()[i])
+    plt.show()
+    print()
+
+
+def plot_categorical_feats_double(dfs, cat_cols=None, ncols=3, height_per_plot=6):
+    """Plot barplots for specified or simply all categorical features in two given DataFrames
+
+    Parameters
+    ----------
+    dfs: list
+        List containing two tuples, each has a DataFrame as its second entry, and the name of
+        the DataFrame as its first entry. See the example below.
+
+    cat_cols: array-like
+        Names of the categorical features to plot. If None, all features stored as
+        object type in the memory will be plotted.
+
+    ncols: int
+        Number of subplots in a row, default 3.
+
+    height_per_plot: int
+        Height of a subplot in the figure, default 6.
+
+    Examples
+    --------
+    >>> df_train = pd.read_csv('train.csv')
+    >>> df_test = pd.read_csv('test.csv')
+    >>> dfs = [('train', df_train), ('test', df_test)]  # two elements indicating two DataFrames and their names
+    >>> plot_categorical_feats_double(dfs)
+    """
+    df1_name, df1 = dfs[0]
+    df2_name, df2 = dfs[1]
+    df_names = "{} and {}".format(df1_name, df2_name)
+    print(df_names)
+    print("-" * len(df_names))
+    if cat_cols is None:
+        cat_cols1 = df1.dtypes[df1.dtypes == object].index.tolist()
+        cat_cols2 = df2.dtypes[df2.dtypes == object].index.tolist()
+        cat_cols = cat_cols1 and cat_cols2
+    num_col_counts = len(cat_cols)
+    nrows = int(np.ceil(num_col_counts / 3))
+    fig, axs = plt.subplots(nrows, ncols, figsize=(20, nrows * height_per_plot))
+    for i, feat in enumerate(cat_cols):
+        ix = i // ncols
+        iy = i % ncols
+        ax = axs[ix][iy] if nrows > 1 else axs[iy]
+        if feat not in df2.columns:
+            plot_categorical_feat(df1[feat], feat, ax, alpha=.5, density=True)
+        elif feat not in df1.columns:
+            plot_categorical_feat(df2[feat], feat, ax, alpha=.5, density=True)
+        else:  # feature appears in both DataFrame
+            vals = np.concatenate([df1[feat].values, df2[feat].values])
+            keys = pd.Series(vals).value_counts().index.tolist()
+            nkeys = len(keys)
+            cmap = get_cmap(max(nkeys + 1, 8))
+            colors = [cmap(n) for n in range(nkeys + 1)]
+            plot_categorical_feat(df1[feat], feat, ax, keys, color=colors, edgecolor='black', width=.4, label=df1_name)
+            plot_categorical_feat(df2[feat], feat, ax, keys, color=colors, edgecolor='black', shift=.4, width=.4,
+                                  label=df2_name)
+            ax.legend([df1_name, df2_name])
+        ax.legend([df1_name, df2_name])
+    for i in range(num_col_counts, nrows * ncols):
         fig.delaxes(axs.flatten()[i])
     plt.show()
     print()
