48ab72d317ca1b605590e02ecd61851b64865984
JanzenLiu <chengtsung_liu@yahoo.com>
2018-04-17 23:24:25 +0800

    real Fix of globals problem

--- code/utils/info_utils.py
+++ code/utils/info_utils.py
@@ -1,12 +1,25 @@
 import types
 
 
+glbs_string = "" \
+              "def globals_wrapper():\n" \
+              " def f():\n" \
+              "     return globals()\n" \
+              " return f"
+
+
+def globals_wrapper():
+    def f():
+        return None
+    return f
+
+
 class InfoUtils:
     """Helper to handle common information
 
     Parameters
     ----------
-    glb: types.FunctionType
+    glbs: types.FunctionType
         Function that returns a global variable dictionary that maps variable names
         to their ids in the memory. If you are using this class by directly importing,
         it's necessary to set this parameter, or the instance will be instantiated with
@@ -15,10 +28,12 @@ class InfoUtils:
 
     Examples
     --------
-    >>> iu = InfoUtils(globals)
+    >>> # from info_utils import InfoUtils, glbs_string, globals_wrapper  # uncomment this during actual use
+    >>> exec(glbs_string)  # Python's globals sucks
+    >>> iu = InfoUtils(globals_wrapper())
     """
-    def __init__(self, glb):
-        self.glb = glb
+    def __init__(self, glbs):
+        self.glbs = glbs
 
     def get_var_name(self, obj) -> str:
         """Get literal name of the given variable.
@@ -35,6 +50,6 @@ class InfoUtils:
         name: string
             Name of the given variable.
         """
-        names = [objname for objname, oid in self.globals().items() if id(oid) == id(obj)]
+        names = [objname for objname, oid in self.glbs().items() if id(oid) == id(obj)]
         name = names[0] if len(names) > 0 else None
         return name
