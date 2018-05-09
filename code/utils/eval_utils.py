588fabb409a1c6c86ebab8f65402d300b2ac15f6
JanzenLiu <chengtsung_liu@yahoo.com>
2018-05-09 21:42:32 +0800

    Add online AUC builder for model callback

--- code/utils/eval_utils.py
+++ code/utils/eval_utils.py
@@ -22,3 +22,33 @@ def online_auc(selector, y_true, y_pred, selector_name="aid", ret_verbose=False)
         return df
     else:
         return aucs.mean()
+
+
+def build_online_auc_eval(train_selectors, valid_selectors):
+    def func(y_true, y_pred):
+        if y_true.shape[0] == train_selectors.shape[0]:
+            return online_auc(selector=train_selectors, y_true=y_true, y_pred=y_pred, ret_verbose=False)
+        elif y_true.shape[0] == valid_selectors.shape[0]:
+            return online_auc(selector=valid_selectors, y_true=y_true, y_pred=y_pred, ret_verbose=False)
+        else:
+            raise ValueError("Mismatched length: {} (train: {}, valid: {})".format(y_true.shape[0],
+                                                                                   train_selectors.shape[0],
+                                                                                   valid_selectors.shape[0]))
+
+    return func
+
+
+def build_lightgbm_online_auc_eval(train_selectors, valid_selectors):
+    def func(y_pred, lgbm_data):
+        y_true = lgbm_data.get_label()
+        if y_true.shape[0] == train_selectors.shape[0]:
+            auc = online_auc(selector=train_selectors, y_true=y_true, y_pred=y_pred, ret_verbose=False)
+        elif y_true.shape[0] == valid_selectors.shape[0]:
+            auc = online_auc(selector=valid_selectors, y_true=y_true, y_pred=y_pred, ret_verbose=False)
+        else:
+            raise ValueError("Mismatched length: {} (train: {}, valid: {})".format(y_true.shape[0],
+                                                                                   train_selectors.shape[0],
+                                                                                   valid_selectors.shape[0]))
+        return "online_auc", auc, True
+
+    return func
